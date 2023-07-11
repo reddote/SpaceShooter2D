6d5b0bd5a78cb8e18b7cb0eb1d7425790c0a7332
DESKTOP-50PQ6DD\L1303 <muhsinalperasar@kist.re.kr>
2023-07-11 14:41:24 +0900

    added fire rate

--- player_controller.py
+++ player_controller.py
@@ -4,9 +4,12 @@ from weapon import WeaponController
 
 
 class PlayerController:
+
     def __init__(self):
         self.bullets = []
         self.movement_speed = 0.1
+        self.last_shot = pygame.time.get_ticks()
+        self.shoot_delay = 500
 
     def player_movement(self, player_pos, screen_pos):
         keys = pygame.key.get_pressed()
@@ -15,9 +18,13 @@ class PlayerController:
         if not player_pos.x > screen_pos and keys[pygame.K_d]:
             player_pos.x += 300 * self.movement_speed
         if keys[pygame.K_SPACE]:
-            temp_pos = player_pos.copy()
-            self.shoot(temp_pos)
+            now = pygame.time.get_ticks()
+            if now - self.last_shot > self.shoot_delay:
+                temp_pos = player_pos.copy()
+                self.shoot(temp_pos)
+                self.last_shot = now
 
     def shoot(self, pos):
         new_bullet = WeaponController(pos)
         self.bullets.append(new_bullet)
+
