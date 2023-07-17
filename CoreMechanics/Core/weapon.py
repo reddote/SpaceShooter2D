bc053133fd4ab7929b03c22ef69161e21efd84cd
DESKTOP-50PQ6DD\L1303 <muhsinalperasar@kist.re.kr>
2023-07-17 14:20:18 +0900

    bullet and npc simple collision detection is added

--- CoreMechanics/Core/weapon.py
+++ CoreMechanics/Core/weapon.py
@@ -5,10 +5,18 @@ class WeaponController:
     def __init__(self, pos):
         self.speed = 3
         self.pos = pos
+        self.bullet_radius = 10
         pass
 
-    def draw(self, surface):
-        pygame.draw.circle(surface, "black", self.pos, 10)
+    def draw(self, surface, rect):
+        pygame.draw.circle(surface, "black", self.pos, self.bullet_radius)
+
+        # Create a Rect with same center as the circle
+        bullet_rect = pygame.Rect(0, 0, self.bullet_radius * 2, self.bullet_radius * 2)
+        bullet_rect.center = (self.pos.x, self.pos.y)
+
+        if bullet_rect.colliderect(rect):
+            print("Collision Detected")
 
     def bullet_move(self):
         self.pos.y -= self.speed
--- main.py
+++ main.py
@@ -14,6 +14,9 @@ dt = 0.1
 player_pos = pygame.Vector2(screen.get_width()/2, 640)
 npc_pos = pygame.Vector2(screen.get_width()/2, 120)
 
+player_radius = 40
+npc_radius = 30
+
 my_instance = PlayerController()
 my_npc_instance = NpcController()
 
@@ -27,12 +30,15 @@ while running:
     # fill the screen with a color to wipe away anything from last frame
     screen.fill("gray")
 
-    pygame.draw.circle(screen, "red", player_pos, 40)
-    pygame.draw.circle(screen, "blue", npc_pos, 30)
+    pygame.draw.circle(screen, "red", player_pos, player_radius)
+    pygame.draw.circle(screen, "blue", npc_pos, npc_radius)
+
+    npc_rect = pygame.Rect(0, 0, npc_radius * 2, npc_radius * 2)
+    npc_rect.center = (npc_pos.x, npc_pos.y)
 
     for bullet in my_instance.bullets:
         bullet.bullet_move()
-        bullet.draw(pygame.display.get_surface())
+        bullet.draw(pygame.display.get_surface(), npc_rect)
 
     my_instance.player_movement(player_pos, screen.get_width())
     my_npc_instance.npc_movement(npc_pos, screen.get_width())
